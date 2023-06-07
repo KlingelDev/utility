a3ba4016c09edde04f374e7847e4aa780142e630
Karl Ruskowski <khruskowski@gmail.com>
2023-06-07 11:01:38 +0200

    Initial commit of compress_folders.sh

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Read Me
+# Utility Scripts
+A number of utility scripts that do a number of things ;)
+
+Everything is released under MIT Licence for details view ```LICENCE```
+
+## Scripts
+### scripts/compress_folder.sh
+Compress folder contents and sync archives to destination (uses rsync and 7z)
+> usage: ./compress_folder.sh [-o DIR] [-c compressdir] [-h] [-d DESTINATION]
---
+++ scripts/compress_folders.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Depends on rsync, 7z
+#
+# Compress folders & files and sync archives to another location
+#
+# https://github.com/Klingel-Dev/utility/README.md
+# https://github.com/Klingel-Dev/utility/LICENCE
+#
+
+if ! command -v rsync > /dev/null; then
+    echo "Depdency: rsync -- This script uses rsync to tranfer files to
+          target directory."
+    exit 1
+fi
+
+if ! command -v 7z > /dev/null; then
+    echo "Depdency: 7z -- This script uses 7z for compression of archives."
+    exit 1
+fi
+
+COMPDIR=.compressed
+
+while getopts ':o:d:c:h' OPTION; do
+    case $OPTION in
+        d)
+            DEST=$OPTARG
+            ;;
+        o)
+            ORIGIN=$OPTARG
+            ;;
+        c)
+            COMPDIR=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option -$OPTION" >&2
+            exit 1
+            ;;
+        *|h)
+            echo "usage: $(basename \$0) DIR [-c compressdir] [-h] DESTINATION" >&2
+            exit 1
+            ;;
+        esac
+done
+
+if [ -z "$ORIGIN" ] && [ -z "$DEST" ]; then
+    echo "Missing origin (-o) and destination (-d)" >&2
+    exit 1
+fi
+
+if [ ! -d $COMPDIR ]; then
+    echo "Creating $COMPDIR"
+    mkdir -p $COMPDIR;
+fi
+
+# TODO Propagate deletions
+for i in $ORIGIN/*
+do
+    if [ ! -f "$COMPDIR/$i.7z" ]; then
+        7z a $COMPDIR/$i.7z $i
+    else
+        7z u $COMPDIR/$i.7z $i -uq0
+    fi
+done
+
+for i in $COMPDIR/*
+do
+    rsync -arptgoD --progress $i $DEST
+done
+
+echo "done."
